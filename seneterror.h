6337b7446f6d2a4d350d1b510af172df0802080e
Cajova-Houba <tkmushroomer@gmail.com>
2016-10-12 17:17:00 +0200

    Repository created. State diagram of server and client added. Messages description added.

---
+++ seneterror.h
@@ -0,0 +1,13 @@
+#ifndef __SENET_ERROR_H
+#define __SENET_ERROR_H
+
+typedef enum senetError {
+	ERR_MSG = 1,
+	ERR_MSG_TYPE,
+	ERR_MSG_CONTENT,
+	ERR_NICKNAME,
+	ERR_NICKNAME_EXIST,
+	ERR_TURN
+}
+
+#endif

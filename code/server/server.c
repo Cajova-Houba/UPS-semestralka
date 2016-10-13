e643a2d23e29bf6556fa16303a8df65d9290e9cf
Cajova-Houba <tkmushroomer@gmail.com>
2016-10-13 13:08:58 +0200

    Base file for server added.

---
+++ code/server/server.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char **argv)
+{
+	int sock, incoming_sock;
+	int optval;
+	
+	printf("I'm a server.\n");
+	
+	/* listen loop */
+	for(;;)
+	{
+		
+	}
+	
+	return 0;
+}

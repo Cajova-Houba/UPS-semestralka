aa7d5a041b76140f630b6ec832514d451c5e1536
Cajova-Houba <tkmushroomer@gmail.com>
2017-01-24 12:04:13 +0100

    Build/run scripts added for client .

--- gitAddAll.sh
+++ gitAddAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-paths=("readme.md" "gitAddAll.sh" "doc/*" "code/server/*.c" "code/server/*.h" "code/server/core/*.h" "code/server/core/*.c" "code/server/common/*.h" "code/server/common/*.c" "code/server/SConstruct" "code/server/CMakeLists.txt" "code/client/src/*" "code/client/pom.xml")
+paths=("readme.md" "gitAddAll.sh" "doc/*" "code/server/*.c" "code/server/*.h" "code/server/core/*.h" "code/server/core/*.c" "code/server/common/*.h" "code/server/common/*.c" "code/server/SConstruct" "code/server/CMakeLists.txt" "code/client/src/*" "code/client/pom.xml" "code/client/*.sh")
 
 for item in ${paths[*]}
 do
